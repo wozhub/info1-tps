519728ace1379b1f54062c02f0d4dc0dacc29455
David Vinazza <davidvinazza@gmail.com>
2014-06-05 19:01:44 -0300

    Cambio de terminologia, estos ejercicios tratan con vectores, no matrices

--- 6/6-7-ordenar.c
+++ 6/6-7-ordenar.c
@@ -6,33 +6,33 @@
 
 #include <malloc.h>     //malloc y free
 #include <stdio.h> // scanf printf
-#include "matrices.h" //funciones de matrices
+#include "vectores.h" //funciones de vectores
 
 void main()
 {
     unsigned int tope, comparaciones,intercambios;
 
-    printf("Ingrese el nro entero de elementos del vector a crear aleatoriamente.\nn=");
+    printf("Ingrese el nro entero de elementos del vector para crearlos aleatoriamente.\nn=");
     scanf("%d",&tope);
 
-    int *matriz = malloc( sizeof(int)*tope );
-    int *auxiliar = malloc( sizeof(int)*tope ); //copia de la matriz para probar los sorts
+    int *vector = malloc( sizeof(int)*tope );
+    int *auxiliar = malloc( sizeof(int)*tope ); //copia de la vector para probar los sorts
 
-    llenarRandom(matriz,tope);
-    copiar(matriz,auxiliar,tope);
-    printf("Matriz Original:\n");
-    ver(matriz,tope);
+    llenarRandom(vector,tope);
+    copiar(vector,auxiliar,tope);
+    printf("Vector Original:\n");
+    ver(vector,tope);
  
     seleccionMinimo(auxiliar,tope,&comparaciones,&intercambios);
     printf("\nOrdenado mediante Selection Sort (minimo) en %d comparaciones y %d intercambios\n",comparaciones,intercambios);
     ver(auxiliar,tope);
     
-    copiar(matriz,auxiliar,tope);
+    copiar(vector,auxiliar,tope);
     burbuja(auxiliar,tope,&comparaciones,&intercambios);
     printf("\nOrdenado mediante Burbujeo en %d comparaciones y %d intercambios\n",comparaciones,intercambios);
     ver(auxiliar,tope);
     
-    copiar(matriz,auxiliar,tope);
+    copiar(vector,auxiliar,tope);
     burbujaMejorada(auxiliar,tope,&comparaciones,&intercambios);
     printf("\nOrdenado mediante Burbujeo Mejorado en %d comparaciones y %d intercambios\n",comparaciones,intercambios);
     ver(auxiliar,tope);
